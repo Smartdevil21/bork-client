e5aac8d761e7465adda2a97295d599dc3a97c26b
Smartdevil21 <prtk.app.dev@gmail.com>
2023-07-14 16:18:10 +0530

    Minor changes

--- src/App.js
+++ src/App.js
@@ -10,121 +10,135 @@ import Stats from "./pages/Habit-Tracker/Stats/Stats";
 import CalenderPage from "./pages/Calender/CalenderPage";
 import Feedback from "./pages/Feedback/Feedback";
 import Home from "./pages/Home/Home";
+import axios from "axios";
 
 const StateContext = createContext();
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(0);
 
-	const [windowWidth, setWindowWidth] = useState(0);
+  const [states, setStates] = useState({
+    user_id: "",
+    username: "",
+    user_email: "",
+    userLoggedIn: false,
 
-	const [states, setStates] = useState({
-		user_id: "",
-		username: "",
-		user_email: "",
-		userLoggedIn: false,
+    // todo States
+    openEditTaskModal: false,
+    openAddTaskModal: false,
+    taskToBeEdited: {},
+    userTasks: [],
+    completedTask: [],
 
-		// todo States
-		openEditTaskModal: false,
-		openAddTaskModal: false,
-		taskToBeEdited: {},
-		userTasks: [],
-		completedTask: [],
+    // Habit Tracker States
+    openAddHabitModal: false,
+    openEditHabitModal: false,
+    openHabitStats: false,
+    activeHabitStat: {},
+    habitToBeEdited: {},
+    userHabits: [],
 
-		// Habit Tracker States
-		openAddHabitModal: false,
-		openEditHabitModal: false,
-		openHabitStats: false,
-		activeHabitStat: {},
-		habitToBeEdited: {},
-		userHabits: [],
+    // Calender States
+    tasksOfSelectedDay: [],
+    dateOfDaySelectedInModal: "",
+  });
 
-		// Calender States
-		tasksOfSelectedDay: [],
-		dateOfDaySelectedInModal: "",
-	});
+  const contextObj = {
+    states,
+    setStates,
+  };
 
-	const contextObj = {
-		states,
-		setStates,
-	};
+  // const testCall = async () => {
+  //   try {
+  //     const result = await axios.get("http://localhost:8000/test");
+  //     console.log(result.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-	useEffect(()=>{
-		setWindowWidth(window.innerWidth);
-		console.log(windowWidth)
-	},[])
+  useEffect(() => {
+    // testCall();
+    setWindowWidth(window.innerWidth);
+    console.log(windowWidth);
+  }, []);
 
-	return (
-		<StateContext.Provider value={contextObj}>
-			{windowWidth>700?<div className="App">
-				<Routes>
-					<Route
-						exact
-						path="/home"
-						element={
-							<div className="main_wrapper">
-								<Sidebar />
-								<Home />
-							</div>
-						}
-					/>
-					<Route
-						exact
-						path="/todo"
-						element={
-							<div className="main_wrapper">
-								<Sidebar />
-								<ToDo />
-							</div>
-						}
-					/>
-					<Route
-						exact
-						path="/habit_tracker"
-						element={
-							<div className="main_wrapper">
-								<Sidebar />
-								<HabitTracker />
-							</div>
-						}
-					/>
-					<Route
-						exact
-						path="/habit_tracker/stats"
-						element={
-							<div className="main_wrapper">
-								<Sidebar />
-								<Stats />
-							</div>
-						}
-					/>
-					<Route
-						exact
-						path="/calender"
-						element={
-							<div className="main_wrapper">
-								<Sidebar />
-								<CalenderPage />
-							</div>
-						}
-					/>
-					<Route
-						exact
-						path="/feedback"
-						element={
-							<div className="feedback_wrapper">
-								{/* <Sidebar /> */}
-								<Feedback />
-							</div>
-						}
-					/>
-					<Route path="/signup" element={<SignUp />}></Route>
-					<Route index element={<Login />}></Route>
-				</Routes>
-			</div>:<div className="sorry">
-				<p>Sorry, this webapp isn't available for mobile devices yet. 😅</p>
-				</div>}
-		</StateContext.Provider>
-	);
+  return (
+    <StateContext.Provider value={contextObj}>
+      {windowWidth > 700 ? (
+        <div className="App">
+          <Routes>
+            <Route
+              exact
+              path="/home"
+              element={
+                <div className="main_wrapper">
+                  <Sidebar />
+                  <Home />
+                </div>
+              }
+            />
+            <Route
+              exact
+              path="/todo"
+              element={
+                <div className="main_wrapper">
+                  <Sidebar />
+                  <ToDo />
+                </div>
+              }
+            />
+            <Route
+              exact
+              path="/habit_tracker"
+              element={
+                <div className="main_wrapper">
+                  <Sidebar />
+                  <HabitTracker />
+                </div>
+              }
+            />
+            <Route
+              exact
+              path="/habit_tracker/stats"
+              element={
+                <div className="main_wrapper">
+                  <Sidebar />
+                  <Stats />
+                </div>
+              }
+            />
+            <Route
+              exact
+              path="/calender"
+              element={
+                <div className="main_wrapper">
+                  <Sidebar />
+                  <CalenderPage />
+                </div>
+              }
+            />
+            <Route
+              exact
+              path="/feedback"
+              element={
+                <div className="feedback_wrapper">
+                  {/* <Sidebar /> */}
+                  <Feedback />
+                </div>
+              }
+            />
+            <Route path="/signup" element={<SignUp />}></Route>
+            <Route index element={<Login />}></Route>
+          </Routes>
+        </div>
+      ) : (
+        <div className="sorry">
+          <p>Sorry, this webapp isn't available for mobile devices yet. 😅</p>
+        </div>
+      )}
+    </StateContext.Provider>
+  );
 }
 
 export default App;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -10,95 +10,95 @@ import { useContext } from "react";
 import { StateContext } from "../../App";
 
 function Login() {
-	const { states, setStates } = useContext(StateContext);
-	const navigator = useNavigate();
-	const [loading, setLoading] = useState(false);
-	const [user, setUser] = useState({
-		username: "",
-		password: "",
-	});
+  const { states, setStates } = useContext(StateContext);
+  const navigator = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
 
-	const loginUser = async (e) => {
-		if (e.key === "Enter" || e.type === "click") {
-			setLoading(true);
-			try {
-				const response = await axios({
-					url: "/userSignIn",
-					method: "post",
-					data: user,
-				});
-				if (response.data.state) {
-					document.querySelector(".login_err").innerHTML = "";
-					setStates((prev) => ({
-						...prev,
-						user_email: response.data.result.email,
-						username: response.data.result.username,
-						userLoggedIn: true,
-					}));
-					alert("Welcome");
-					navigator(`/home?user_id=${response.data.result._id}`);
-				} else {
-					document.querySelector(".login_err").innerHTML =
-						"Incorrect Username or Password!";
-				}
-			} catch (error) {
-				console.error(error);
-			}
-			setLoading(false);
-		}
-	};
+  const loginUser = async (e) => {
+    if (e.key === "Enter" || e.type === "click") {
+      setLoading(true);
+      try {
+        // url: "http://localhost:8000/userSignIn",
+        //   method: "post",
+        //   data: user,
+        console.log(user)
+        const response = await axios.post("/userSignIn", user);
+        if (response.data.state) {
+          document.querySelector(".login_err").innerHTML = "";
+          setStates((prev) => ({
+            ...prev,
+            user_email: response.data.result.email,
+            username: response.data.result.username,
+            userLoggedIn: true,
+          }));
+          alert("Welcome");
+          navigator(`/home?user_id=${response.data.result._id}`);
+        } else {
+          document.querySelector(".login_err").innerHTML =
+            "Incorrect Username or Password!";
+        }
+      } catch (error) {
+        console.error(error);
+      }
+      setLoading(false);
+    }
+  };
 
-	return (
-		<div className={Styles.login} onKeyDown={loginUser} tabIndex="0">
-			<div className={Styles.login_wrapper}>
-				<div className={Styles.login_img}>
-					<img src={login} alt="login_img" />
-				</div>
-				<div className={Styles.login_inputs}>
-					<Stack direction={"column"} spacing={2}>
-						<h1>Sign In...</h1>
-						<TextField
-							variant="standard"
-							size="small"
-							label="Username"
-							value={user.username}
-							onChange={(e) => {
-								setUser((prev) => ({ ...prev, username: e.target.value }));
-							}}
-							type="text"
-						/>
-						<TextField
-							variant="standard"
-							size="small"
-							value={user.password}
-							onChange={(e) => {
-								setUser((prev) => ({ ...prev, password: e.target.value }));
-							}}
-							label="Password"
-							type="password"
-						/>
-						<p
-							className="login_err"
-							style={{ color: "red", fontSize: "10px", fontWeight: "600" }}
-						></p>
-						<div className={Styles.bottom_btns}>
-							<Button onClick={loginUser}>
-								{loading ? "loading" : "Sign-In"}
-							</Button>
-						</div>
-						<Stack>
-							<p className={Styles.sign_up_link}>
-								Don't have an account?{" "}
-								<NavLink exact="true" to="/signup">
-									Create one
-								</NavLink>
-							</p>
-						</Stack>
-					</Stack>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={Styles.login} onKeyDown={loginUser} tabIndex="0">
+      <div className={Styles.login_wrapper}>
+        <div className={Styles.login_img}>
+          <img src={login} alt="login_img" />
+        </div>
+        <div className={Styles.login_inputs}>
+          <Stack direction={"column"} spacing={2}>
+            <h1>Sign In...</h1>
+            <TextField
+              variant="standard"
+              size="small"
+              label="Username"
+              value={user.username}
+              onChange={(e) => {
+                setUser((prev) => ({ ...prev, username: e.target.value }));
+              }}
+              type="text"
+            />
+            <TextField
+              variant="standard"
+              size="small"
+              value={user.password}
+              onChange={(e) => {
+                setUser((prev) => ({ ...prev, password: e.target.value }));
+              }}
+              label="Password"
+              type="password"
+            />
+            <p
+              className="login_err"
+              style={{ color: "red", fontSize: "10px", fontWeight: "600" }}
+            ></p>
+            <div className={Styles.bottom_btns}>
+              <Button onClick={loginUser}>
+                {loading ? "loading" : "Sign-In"}
+              </Button>
+            </div>
+            <Stack>
+              <p className={Styles.sign_up_link}>
+                Don't have an account?{" "}
+                <NavLink exact="true" to="/signup">
+                  Create one
+                </NavLink>
+              </p>
+            </Stack>
+          </Stack>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
